4e3097bae7ab4350aade58f040916828f58c4af5
Xarakternuk <Xarakternik@gmail.com>
2011-05-28 10:19:40 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yasuk::Application.config.secret_token = '767afbac4a4eb434a04beb1f7126336be6693e463c238e27b0752335646ad5fdc20616b42dbb04ca275386ba2bdba228bb6dc98cdf9502f4a8e1c27c8e78951f'
